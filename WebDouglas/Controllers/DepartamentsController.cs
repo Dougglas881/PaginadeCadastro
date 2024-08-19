5c1ff2c11243bbb3688488cfec086f36f32f0707
Douglas Ronaldo <douglasronaldo881@yaoo.com.br>
2024-08-19 18:37:50 -0300

    Foi criada um pagina

---
+++ WebDouglas/Controllers/DepartamentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebDouglas.Models;
+using WebDouglas.Models;
+
+public class DepartamentsController : Controller
+{
+    public IActionResult Index()
+    {
+        List<Departament> list = new List<Departament>();
+        list.Add(new Departament {Id = 1, Name ="Eletronics" });
+        list.Add(new Departament { Id = 2, Name = "Fashion" });
+
+
+        return View(list);
+    }
+}
